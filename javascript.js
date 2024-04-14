1f6e2d4dc6b0889896dcb7feb354db8118a1ff93
Ismael <ismacm95@gmail.com>
2024-04-14 18:27:34 +0100

    Creation of playRound where the system gets player input and compareswith the computer selection and decides who win.

--- javascript.js
+++ javascript.js
@@ -3,24 +3,46 @@
 function getComputerChoice() {
     //Create a random number
     let pickIndex = Math.random();
-    console.log(pickIndex);
 
     //Give ranges to the different choices so it use the random number
     let computerChoice;
 
     if (pickIndex < 0.33) {
-        computerChoice = "Rock";
+        computerChoice = "rock";
     }
 
     else if (pickIndex < 0.66) {
-        computerChoice = "Paper";
+        computerChoice = "paper";
     }
 
     else {
-        computerChoice = "Scissors";
+        computerChoice = "scissors";
     }
-    console.log(computerChoice);
-    
+
+    return computerChoice;
 }
 
-getComputerChoice();
+//Create a new function to compare user input and ComputerChoice and share who won based on RPS rules
+function playRound(playerSelection, computerSelection){
+    //gather user input and make it case insensitive
+    playerSelection = playerSelection.toLowerCase();
+    
+    //selection comparison and winner logic
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie! " + playerSelection + " vs. " + computerSelection);
+    }
+
+    else {
+        if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
+            console.log("You win! " + playerSelection + " beats " + computerSelection);
+        }
+
+        else {
+            console.log("You lose! " + computerSelection + " beats " + playerSelection);
+        }
+    }
+}
+
+let playerSelection = prompt("Rock, paper or scissors?");
+
+playRound(playerSelection, getComputerChoice());
