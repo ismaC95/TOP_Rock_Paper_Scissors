c12338ed442d5368509e6ff8968436c656d519c8
Ismael <ismacm95@gmail.com>
2024-06-28 11:30:53 +0100

    Adding all new features from Revisiting Rock, Paper, Scissors

--- javascript.js
+++ javascript.js
@@ -22,48 +22,106 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-//Create a new function to compare user input and ComputerChoice and share who won based on RPS rules
-function playRound(playerSelection, computerSelection){
-    //gather user input and make it case insensitive
-    playerSelection = playerSelection.toLowerCase();
-    
-    //selection comparison and winner logic
-    if (playerSelection === computerSelection) {
-        console.log("It's a tie! " + playerSelection + " vs. " + computerSelection);
-    }
 
-    else {
-        if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
-            console.log("You win! " + playerSelection + " beats " + computerSelection);
-            
-            return playerRound++;
-        }
-
-        else {
-            console.log("You lose! " + computerSelection + " beats " + playerSelection);
-
-            return computerRound++;
-        }
-    }
-}
 
 let playerRound = 0;
 
 let computerRound = 0;
 
-//Function to play 5 rounds
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        //request input from player every round
-        let playerSelection = prompt("Rock, paper or scissors?");
+//Create a new function to compare user input and ComputerChoice and share who won based on RPS rules
+function playRound(playerSelection, computerSelection){
+    //gather user input and make it case insensitive
+    playerSelection = playerSelection.toLowerCase();
+    const resultRow = document.querySelector("#result");
+    //selection comparison and winner logic
+    if (playerSelection === computerSelection) {
+        //create a new row with tie
+        let drawRound = document.createElement("p");
 
-        playRound(playerSelection, getComputerChoice());
+        resultRow.appendChild(drawRound);
 
-        //counting total rounds
-        console.log("Player: " + playerRound + ". Computer: " + computerRound);
+        drawRound.textContent = "It's a tie! " + playerSelection + " vs. " + computerSelection;
     }
-    //at the end of the game, message to say who was the winner and the final results
-    (playerRound > computerRound) ? console.log("Congratulations, you won! The final score is: Player " + playerRound + " to Computer " + computerRound + ".") : console.log("Sorry, computer won. Better luck next time!The final score is: Player " + playerRound + " to Computer " + computerRound + ".");
+    else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
+        //create a new row with win
+        let winRound = document.createElement("p");
+
+        resultRow.appendChild(winRound);
+
+        winRound.textContent = "You win! " + playerSelection + " beats " + computerSelection;
+
+        playerRound++;
+
+        console.log("player round is = " + playerRound);
+    }
+    else {
+        //create a new row with lose
+        let loseRound = document.createElement("p");
+
+        resultRow.appendChild(loseRound);
+
+        loseRound.textContent = "You lose! " + computerSelection + " beats " + playerSelection
+
+        computerRound++;
+
+        console.log("computer round is " + computerRound);
+    }
+    checkWinner();
 }
 
-playGame();
+//Announce winner
+function checkWinner(){
+    if(playerRound == 5 || computerRound == 5){
+        let winner = document.createElement("p");
+        result.appendChild(winner);
+        if(playerRound == 5){
+            winner.textContent = `Congrats! You won ${playerRound} to ${computerRound}!`;
+        }
+        else{
+            winner.textContent = `Better luck next time. You lost ${playerRound} to ${computerRound}!`;
+        }
+        document.querySelectorAll("button").forEach(button => button.disabled = true);
+    }
+}
+
+//Creating UI
+
+//Three buttons with each selection
+document.addEventListener("DOMContentLoaded", () => {
+const btnContainer = document.querySelector("#btnContainer");
+
+const rockBtn = document.createElement("button");
+rockBtn.textContent = "Rock";
+btnContainer.appendChild(rockBtn);
+
+const paperBtn = document.createElement("button");
+paperBtn.textContent = "Paper";
+btnContainer.appendChild(paperBtn);
+
+const scissorsBtn = document.createElement("button");
+scissorsBtn.textContent = "Scissors";
+btnContainer.appendChild(scissorsBtn);
+
+//DOM from results:
+let scores = document.createElement("p");
+
+scores.textContent = "Player: " + playerRound + " rounds - Computer: " + computerRound + " rounds.";
+
+const totalResult = document.querySelector("#result");
+
+totalResult.appendChild(scores);
+
+//click event in each button
+rockBtn.addEventListener("click", () => {
+    playRound("rock", getComputerChoice())
+    scores.textContent = "Player: " + playerRound + " rounds - Computer: " + computerRound + " rounds.";
+});
+paperBtn.addEventListener("click", () =>{
+    playRound("paper", getComputerChoice())
+    scores.textContent = "Player: " + playerRound + " rounds - Computer: " + computerRound + " rounds.";
+});
+scissorsBtn.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice())
+    scores.textContent = "Player: " + playerRound + " rounds - Computer: " + computerRound + " rounds.";
+});
+});
